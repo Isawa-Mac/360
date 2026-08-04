d58bb94cb24984b8157eba660a5ac3b367e822fd
Isawa-Mac <isawa.mac@gmail.com>
2026-08-04 08:29:27 +0700

    Update mobile app APK download URL

--- app/download/mobile-app/route.ts
+++ app/download/mobile-app/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
-const CURRENT_APK_URL = "https://trirexinter-my.sharepoint.com/:u:/g/personal/isara-it_trirex_co_th/IQAxMBs-h9-uRb-n5vYwDEjJAayJhhzmKF6YQkgNd5-oSGg?e=K3gq4I"
+const CURRENT_APK_URL = "https://trirexinter-my.sharepoint.com/:u:/g/personal/isara-it_trirex_co_th/IQACLEnaHwX1RK-iE-GFwJCFAQCJEZu3pQoffO6eMC0lO5A?e=TJ7R8M"
 
 export function GET() {
   return NextResponse.redirect(CURRENT_APK_URL, 302)
