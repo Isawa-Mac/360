a50f2825f13a2bbf4b7f17b2cafa50eb3a2ea9f5
Isawa-Mac <isawa.mac@gmail.com>
2026-08-07 12:08:25 +0700

    Refine app menu icon styling

--- components/app-menu.tsx
+++ components/app-menu.tsx
@@ -11,9 +11,9 @@ export function AppMenu({ className }: { className?: string }) {
     <Link
       href="/home"
       className={cn(
-        "flex items-center justify-start shrink-0 rounded-lg border border-border/50 bg-muted/30 hover:bg-muted/60 transition-colors",
-        "h-10 w-10 pl-2",
-        "group-data-[collapsible=icon]:h-9 group-data-[collapsible=icon]:w-9 group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:pl-0",
+        "flex shrink-0 items-center justify-center p-0 transition-opacity hover:opacity-80",
+        "h-10 w-10",
+        "group-data-[collapsible=icon]:h-10 group-data-[collapsible=icon]:w-10",
         className
       )}
       aria-label={t("go_to_360")}
@@ -24,7 +24,7 @@ export function AppMenu({ className }: { className?: string }) {
       <img
         src="/icons/icon-512.png?v=8"
         alt="360 Intelligent"
-        className="h-7 w-7 object-contain group-data-[collapsible=icon]:h-6 group-data-[collapsible=icon]:w-6"
+        className="h-10 w-10 object-contain"
       />
     </Link>
   )
